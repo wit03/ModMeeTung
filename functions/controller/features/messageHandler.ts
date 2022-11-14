02cf744cb82483c1e97a124ea7494762def09b24
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-11-14 23:04:24 +0700

    wrench: temporary remove transactionLogHandler

--- functions/controller/features/messageHandler.ts
+++ functions/controller/features/messageHandler.ts
@@ -6,13 +6,14 @@ import transactionLogHandler from "./transactionLogHandler"
 const messageHandler = (event) => {
     var msg = event.message.text
     var token = event.replyToken
-    if (msg.match(/^([\d.]+)(j?)([fdtmsola])$/i)) { //match transaction record
-        return transactionLogHandler(msg, token)
-    } else if (msg.match(/สรุป/g)) {
+    // if (msg.match(/^([\d.]+)(j?)([fdtmsola])$/i)) { //match transaction record
+    //     return transactionLogHandler(msg, token)
+    // } else 
+    if (msg.match(/สรุป/g)) {
         return summaryHandler(msg, token)
     } else if (msg.match(/วิธีการใช้งาน/g)) {
         return howToHandler(msg, token)
-    }else {
+    } else {
         return client.replyMessage(token, {
             type: "text",
             text: "ขอโทษครับ, เรายังไม่เข้าใจสิ่งที่คุณพิมพ์",
