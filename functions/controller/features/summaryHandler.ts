a34575aff067f3a18c9f58bf538599889294e677
wit03 <witjarukit@gmail.com>
2022-12-18 23:38:56 +0700

    :fire: firefire

--- functions/controller/features/summaryHandler.ts
+++ functions/controller/features/summaryHandler.ts
@@ -1,6 +1,45 @@
 import { client } from "../../router"
+import Airtable from "airtable";
 
-const summaryHandler = (msg, token) => {
+const summaryHandler = async (msg, token) => {
+    var dailyGoal = 200;
+    var monthlyGoal = 4000;
+
+    var date = new Date().toISOString().split("T")[0];
+    var month = new Date().toISOString().split("T")[0].split("-")[1];
+
+    var dailySpending = 0;
+    var monthlySpending = 0;
+
+    const ATbase = new Airtable({
+        apiKey: process.env.AIRTABLE_API,
+    }).base((process.env.AIRTABLE_BASE)!);
+
+    async function getSpending() {
+        var data: any = [];
+        const records = await ATbase("Expense Tracker").select().all();
+        records.forEach((record) => {
+            data.push({
+                date: record.get("Date"),
+                amount: record.get("Amount"),
+            });
+        });
+
+        var dateTable = data.filter((list) => list.date === date); //filter data by date
+        var monthTable = data.filter((list) => list.date.split("-")[1] == month) // filter data by month
+        var todaySpending = dateTable.map((item) => item.amount).reduce((prev, curr) => prev + curr, 0,); //some spending
+        var monthSpending = monthTable.map((item) => item.amount).reduce((prev, curr) => prev + curr, 0,); //some spending
+        return { todaySpending, monthSpending };
+    }
+
+    await getSpending().then((res) => {
+        dailySpending = res.todaySpending;
+        monthlySpending = res.monthSpending;
+    });
+
+
+
+
     var result: any = {
         type: "carousel",
         contents: [
@@ -13,7 +52,7 @@ const summaryHandler = (msg, token) => {
                     contents: [
                         {
                             type: "text",
-                            text: "In Progress",
+                            text: "Daily",
                             color: "#ffffff",
                             align: "start",
                             size: "md",
@@ -21,7 +60,7 @@ const summaryHandler = (msg, token) => {
                         },
                         {
                             type: "text",
-                            text: "70%",
+                            text: JSON.stringify((dailySpending/dailyGoal) * 100),
                             color: "#ffffff",
                             align: "start",
                             size: "xs",
@@ -65,7 +104,7 @@ const summaryHandler = (msg, token) => {
                             contents: [
                                 {
                                     type: "text",
-                                    text: "Buy milk and lettuce before class",
+                                    text: JSON.stringify(dailySpending) + " " + "฿",
                                     color: "#8C8C8C",
                                     size: "sm",
                                     wrap: true
@@ -92,7 +131,7 @@ const summaryHandler = (msg, token) => {
                     contents: [
                         {
                             type: "text",
-                            text: "In Progress",
+                            text: "Monthly",
                             color: "#ffffff",
                             align: "start",
                             size: "md",
@@ -100,7 +139,7 @@ const summaryHandler = (msg, token) => {
                         },
                         {
                             type: "text",
-                            text: "100%",
+                            text: JSON.stringify(monthlySpending) + " " + "฿",
                             color: "#ffffff",
                             align: "start",
                             size: "xs",
@@ -144,7 +183,7 @@ const summaryHandler = (msg, token) => {
                             contents: [
                                 {
                                     type: "text",
-                                    text: "Buy milk and lettuce before class",
+                                    text: JSON.stringify(monthlySpending) + " " + "฿",
                                     color: "#8C8C8C",
                                     size: "sm",
                                     wrap: true
