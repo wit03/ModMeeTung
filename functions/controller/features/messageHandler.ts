9553afba869d3306d06cbd4c083243b2f81fd2aa
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2022-12-18 17:56:11 +0700

    feat: tutorialHandler

--- functions/controller/features/messageHandler.ts
+++ functions/controller/features/messageHandler.ts
@@ -2,12 +2,15 @@ import { client } from "../../router"
 import noContextHandler from "./noContextHandler"
 import summaryHandler from "./summaryHandler"
 import transactionLogHandler from "./transactionLogHandler"
+import tutorialHandler from "./tutorialHandler"
 
 const messageHandler = (event) => {
     var msg = event.message.text
     var token = event.replyToken
     if (msg.match(/^([\d.]+)(j?)([fdtmsola])$/i)) { //match transaction record
         return transactionLogHandler(msg, token)
+    } else if (msg.match(/ใช้งาน/g)) {
+        return tutorialHandler(msg, token)
     } else if (msg.match(/สรุป/g)) {
         return summaryHandler(msg, token)
     } else {
---
+++ functions/controller/features/tutorialHandler.ts
@@ -0,0 +1,11 @@
+import { client } from "../../router"
+
+const tutorialHandler = (msg, token) => {
+    const result = "วิธีการใช้งานคำสั่งต่าง ๆ\n 50f - บันทึกจำนวนเงินเเละหมวดหมู่ของรายจ่าย\n เเสดงหมวดหมู่ - เเสดงหมวดหมู่รายจ่ายทั้งหมด \n เป้าหมาย - เเสดงเป้าหมายรายวันเเละรายเดือน ณ ปัจจุบัน"
+    return client.replyMessage(token, {
+        type: "text",
+        text: result
+    })
+}
+
+export default tutorialHandler
